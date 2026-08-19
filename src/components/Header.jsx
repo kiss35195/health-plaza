8096caf3e0985988f296fb9f87f1fadbe1f08806
kiss <vicky35195@gmail.com>
2026-08-19 16:10:23 +0800

    小螢幕可點擊開啟或關閉選單

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <header className="flex flex-col bg-lime-800 p-3 font-bold text-lime-200 md:tracking-widest lg:flex-row">
       <div className="mx-5 flex flex-row items-center justify-between md:basis-1/3">
         <Link to="/" className="cactus-classical-serif-regular text-3xl">
           健康廣場
         </Link>
+        <button className="md:hidden" onClick={() => setIsOpen((p) => !p)}>
+          {isOpen ? "▴關閉" : "▾開啟"}選單
+        </button>
       </div>
-      <div className="flex flex-col items-center md:basis-2/3 md:flex-row md:justify-end">
+      <div
+        className={`${isOpen ? "flex" : "hidden"} md:flex flex-col items-center md:basis-2/3 md:flex-row md:justify-end`}
+      >
         <div className="flex gap-2 py-2 md:gap-10 md:py-0 md:pr-10 md:text-lg">
           <Link to="/menu">美食介紹</Link>
           <Link to="/doit">復刻美食</Link>
